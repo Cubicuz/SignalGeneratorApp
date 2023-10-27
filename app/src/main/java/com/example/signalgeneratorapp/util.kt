82093b5aa279399755c4101ad862307997d9b949
cubicuz <michaelschmelcher@t-online.de>
2023-10-27 18:14:28 +0200

    slowly change from java to kotlin

--- app/src/main/java/com/example/signalgeneratorapp/util.kt
+++ app/src/main/java/com/example/signalgeneratorapp/util.kt
@@ -1,41 +1,36 @@
-package com.example.signalgeneratorapp;
+package com.example.signalgeneratorapp
 
-import java.util.*;
+import android.hardware.Sensor
 
-import static android.hardware.Sensor.*;
+object util {
+    @JvmField
+    val SensorsWithNegativeRange: Set<Int> = setOf(
+            Sensor.TYPE_ACCELEROMETER,
+            Sensor.TYPE_GRAVITY,
+            Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR,
+            Sensor.TYPE_GYROSCOPE,
+            Sensor.TYPE_MAGNETIC_FIELD,
+            Sensor.TYPE_ACCELEROMETER,
+            Sensor.TYPE_LINEAR_ACCELERATION,
+            Sensor.TYPE_ROTATION_VECTOR
+    )
 
-public class util {
-    public static final Set<Integer> SensorsWithNegativeRange = new TreeSet<Integer>(Arrays.asList(TYPE_ACCELEROMETER,
-            TYPE_GRAVITY,
-            TYPE_GEOMAGNETIC_ROTATION_VECTOR,
-            TYPE_GYROSCOPE,
-            TYPE_MAGNETIC_FIELD,
-            TYPE_ACCELEROMETER,
-            TYPE_LINEAR_ACCELERATION,
-            TYPE_ROTATION_VECTOR
-    ));
-
-    public static final Map<Integer, Integer> SensorDimensions = Map.ofEntries(
-            // Motion Sensors
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_ACCELEROMETER, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_GRAVITY, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_GYROSCOPE, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_LINEAR_ACCELERATION, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_ROTATION_VECTOR, 4),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_STEP_COUNTER, 1),
-
-            // Position Sensors
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_GAME_ROTATION_VECTOR, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_GEOMAGNETIC_ROTATION_VECTOR, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_MAGNETIC_FIELD, 3),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_PROXIMITY, 1),
-
-            // Environment Sensor
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_AMBIENT_TEMPERATURE, 1),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_LIGHT, 1),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_PRESSURE, 1),
-            new AbstractMap.SimpleEntry<Integer, Integer>(TYPE_RELATIVE_HUMIDITY, 1)
-    );
-
-    public static final String INTENT_SIGNAL_NAME = "intent_signal_name";
+    @JvmField
+    val SensorDimensions = mapOf( // Motion Sensors
+        Sensor.TYPE_ACCELEROMETER to 3,
+        Sensor.TYPE_GRAVITY to 3,
+        Sensor.TYPE_GYROSCOPE to 3,
+        Sensor.TYPE_LINEAR_ACCELERATION to 3,
+        Sensor.TYPE_ROTATION_VECTOR to 4,
+        Sensor.TYPE_STEP_COUNTER to 1,  // Position Sensors
+        Sensor.TYPE_GAME_ROTATION_VECTOR to 3,
+        Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR to 3,
+        Sensor.TYPE_MAGNETIC_FIELD to 3,
+        Sensor.TYPE_PROXIMITY to 1,  // Environment Sensor
+        Sensor.TYPE_AMBIENT_TEMPERATURE to 1,
+        Sensor.TYPE_LIGHT to 1,
+        Sensor.TYPE_PRESSURE to 1,
+        Sensor.TYPE_RELATIVE_HUMIDITY to 1
+    )
+    const val INTENT_SIGNAL_NAME = "intent_signal_name"
 }
